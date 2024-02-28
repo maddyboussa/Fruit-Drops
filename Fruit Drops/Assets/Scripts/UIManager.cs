4c6392153ad51e9d7c9cfcbe65abdd9f3b6e349a
maddyboussa <mnb7937@g.rit.edu>
2024-02-27 21:51:56 -0500

    implemented primative pause ui

--- Fruit Drops/Assets/Scripts/UIManager.cs
+++ Fruit Drops/Assets/Scripts/UIManager.cs
@@ -10,12 +10,18 @@ public class UIManager : MonoBehaviour
     // reference to score text
     [SerializeField] private TMP_Text scoreText;
 
+    // reference to pause menu
+    [SerializeField] private GameObject pauseMenu;
+    private bool paused;
+
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // deactivate pause menu on start
+        pauseMenu.SetActive(false);
+        paused = false;
     }
 
     // Update is called once per frame
@@ -23,6 +29,43 @@ public class UIManager : MonoBehaviour
     {
         
     }
+
+    public void Pause()
+    {
+        // first check for game over -- skipping for now
+        // if game is not over, verify game is not already paused
+        if (paused)
+        {
+            // if so, resume game
+            Resume();
+        }
+        else
+        {
+            // if unpaused, pause the game and activate the pause menu
+            // activate pause menu canvas
+            pauseMenu.SetActive(true);
+
+            // make sure cursor is visible
+            //Cursor.visible = true;
+
+            // freeze time
+            Time.timeScale = 0.0f;
+
+            paused = true;
+        }
+    }
+
+    public void Resume()
+    {
+        // deactivate pause menu
+        pauseMenu.SetActive(false);
+
+        // unfreeze time
+        Time.timeScale = 1.0f;
+
+        paused = false;
+    }
+
 
     public void OnScoreChanged(Component sender, object scoreData)
     {
